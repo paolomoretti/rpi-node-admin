e2bcd2e3a4be5a667a4345f0b6dca4959b570f03
Paolo Moretti <littlebrown@gmail.com>
2014-04-17 16:23:08 +0100

    Added public and assets folder

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var connect = require('connect');
 connect.createServer(
-    connect.static(__dirname)
+    connect.static(__dirname + "/public")
 ).listen(9999);
+
+console.log("Sounds like I'm running from " + __dirname + ", goooooood!");
